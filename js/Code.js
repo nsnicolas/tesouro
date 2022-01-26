e2c7266a7bcbf76b4b4d529eb592906c6a4c7c23
nsnicolas <88857304+nsnicolas@users.noreply.github.com>
2022-01-26 16:33:00 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("N U T F N O I C",700,150)
+    fill("lightblue")
+    text("Dica: Realize uma tarefa em particular",700,170)
+    
+    fill("white")
+    textSize(15)
+    text("T A S D E A B A",100,250)
+    fill("lightblue")
+    text("Dica: Guarda todas as informacoes",100,270)
+}
